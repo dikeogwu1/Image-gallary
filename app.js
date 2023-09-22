c9d763f34c8e52000ed2e86f0834aa807a25c680
fabian <godsonhm3@gmail.com>
2023-09-22 11:58:52 +0100

    add api documentation

--- app.js
+++ app.js
@@ -29,12 +29,13 @@ app.use(
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   })
 );
-app.use(express.json());
+app.use(express.json("./public"));
 // extra packages
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.use(express.static());
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/image", imageRouter);
